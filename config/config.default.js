c9a25b5af74881b79ef52779a0ea80b2d2ec5a28
William Emfinger <waemfinger@gmail.com>
2017-05-11 14:59:07 -0700

    updated for initial version of HFSM Visualizer for showing the heirarchy of the FSM.

--- config/config.default.js
+++ config/config.default.js
@@ -10,6 +10,11 @@ config.seedProjects.basePaths = [__dirname + '/../src/seeds'];
 
 config.requirejsPaths.hfsm = "./src/common";
 
+config.requirejsPaths['webgme-to-json'] = "./node_modules/webgme-to-json";
+config.requirejsPaths['cytoscape'] = "./node_modules/cytoscape/dist"
+config.requirejsPaths['handlebars'] = "./node_modules/handlebars/"
+config.requirejsPaths['cytoscape-cose-bilkent'] = "./node_modules/cytoscape-cose-bilkent/"
+
 config.authentication.enable = true;
 config.authentication.allowGuests = true;
 
