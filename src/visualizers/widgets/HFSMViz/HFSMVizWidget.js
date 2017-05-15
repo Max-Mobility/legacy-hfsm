3cea43537a7ef15e05ef017f0c78574a619c3a9e
William Emfinger <waemfinger@gmail.com>
2017-05-15 16:21:59 -0700

    updated to have HFSMViz resize when parent resizes.

--- src/visualizers/widgets/HFSMViz/HFSMVizWidget.js
+++ src/visualizers/widgets/HFSMViz/HFSMVizWidget.js
@@ -280,7 +280,7 @@ define([
 	};
 
 	HFSMVizWidget.prototype.onWidgetContainerResize = function (width, height) {
-            this._logger.debug('Widget is resizing...');
+	    this._cy.resize();
 	};
 
 	HFSMVizWidget.prototype.checkDependencies = function(desc) {
